f02614ca01801b2b9d68ff36cfdf3350c6e2bc5c
aiiiden <hjjam100@gmail.com>
2022-09-18 15:10:17 +0900

    Init structure with next + tailwind + recoil

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { RecoilRoot } from 'recoil'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
+}
+
+export default App
