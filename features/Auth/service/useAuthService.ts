68706021920e6640fb897b40959da14a3d703e4d
aiiiden <hjjam100@gmail.com>
2022-09-24 14:59:34 +0900

    Add Login Feature with RHF

--- features/Auth/service/useAuthService.ts
+++ features/Auth/service/useAuthService.ts
@@ -1,12 +1,19 @@
+import useEnv from '@/hooks/useEnv'
+import Requester from '@/utils/requester'
+import { useState } from 'react'
+
 /**
  * Hook for Authentication with API call
  * @example login, logout, refresh token, validate token, ...
  */
 export default function useAuthService() {
-  const login = async (payload: Auth.Login.Payload) => {
-    // TODO: API Call & Logic
-    alert(`Welcome! ${payload.email}`)
+  const loginWithEmailAddress = async (payload: Auth.Login.Payload) => {
+    const { data, status } = await Requester.post<Auth.Login.Result>(
+      '/Client/LoginWithEmailAddress',
+      payload,
+    )
+    return data
   }
 
-  return { login }
+  return { loginWithEmailAddress }
 }
--- types/auth.d.ts
+++ types/auth.d.ts
@@ -1,15 +1,46 @@
-interface LoginPayload {
-  email: string
-  password: string
-}
-interface LoginFormData {
-  email: string
-  password: string
-}
+import { LoginFormData } from '@/features/Auth/utils/loginFormSchema'
 
-declare namespace Auth {
-  namespace Login {
-    type FormData = LoginFormData
-    type Payload = LoginPayload
+/**
+ * The response of the login request
+ * @see https://learn.microsoft.com/en-us/rest/api/playfab/client/authentication/login-with-email-address?view=playfab-rest
+ */
+type LoginResult = PlayFab.Response<{
+  SessionTicket: string
+  PlayFabID: string
+  NewlyCreated: boolean
+  SettingsForUser: {
+    NeedsAttribution: boolean
+    GatherDeviceInfo: boolean
+    GatherFocusInfo: boolean
+  }
+  LastLoginTime: Date
+  EntityToken: {
+    EntityToken: string
+    TokenExpiration: Date
+    Entity: {
+      Id: string
+      Type: string
+      TypeString: string
+    }
+  }
+  treatmentAssignment: unknown[]
+}>
+
+declare global {
+  declare namespace Auth {
+    namespace Login {
+      /**
+       * Form data for login
+       */
+      type FormData = LoginFormData
+      /**
+       * Payload for loginWithEmailAddress
+       */
+      type Payload = LoginFormData & { titleId: string }
+      /**
+       * Response of loginWithEmailAddress
+       */
+      type Result = LoginResult
+    }
   }
 }
